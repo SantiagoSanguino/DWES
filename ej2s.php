bdc51b36e0c1c7ff365f0ca91b500cb8ef6b0b49
SantiagoSanguino <91522514+SantiagoSanguino@users.noreply.github.com>
2021-09-28 08:55:15 +0200

    Update ej2s.php
    Cambio, para calcular correctamente la conversión de decimal a binario

--- ej2s.php
+++ ej2s.php
@@ -5,7 +5,25 @@
 <h2>Ejercicio 2 Strings y sus funciones</h2>
 <?php
 	$ip="192.168.206.214";
-	echo "La Ip ".$ip." en decimal seria ".base_convert($ip,10,2)."<br/>";
+	$ipAux=$ip;
+	$ip1=directionPoints($ipAux);
+	$ipAux=directionIp($ipAux);
+	$ip2=directionPoints($ipAux);
+	$ipAux=directionIp($ipAux);
+	$ip3=directionPoints($ipAux);
+	$ipAux=directionIp($ipAux);
+	$ip4=$ipAux;
+	function directionIp($ip) {
+		$ip=strchr($ip,".",false);
+		return substr($ip,1);
+	}
+	function directionPoints($ip) {
+		return strchr($ip,".",true);
+	}
+	function convertirABinario($ip1,$ip2,$ip3,$ip4){
+		return base_convert($ip1,10,2).".".base_convert($ip2,10,2).".".base_convert($ip3,10,2).".".base_convert($ip4,10,2);
+	}
+	echo "La Ip ".$ip." en decimal seria ".convertirABinario($ip1,$ip2,$ip3,$ip4)."<br/>";
 	# Antiguo codigo
 	//$ip="192168206214";
 	//$ipbin=decbin($ip);
