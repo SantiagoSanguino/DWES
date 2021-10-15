ebed12d97f4f12d951d80132b499c24b7f2db0d3
SantiagoSanguino <91522514+SantiagoSanguino@users.noreply.github.com>
2021-10-15 14:00:29 +0200

    Add files via upload

--- Retos/R21_bingo.php
+++ Retos/R21_bingo.php
@@ -18,59 +18,98 @@ echo "
 <BODY>
 <h2> Reto 1 Bingo </h2>";
 	//Actualizo el bingo para contener funciones
-	function generarCarton(){
-		
+	function genNumAle(){
+		return rand(1,60);
 	}
+	function genCarton(&$arrayCarton,$maximo){
+		$i=0;
+		//Codigo actual sin boolean
+		while(count($arrayCarton)<$maximo){
+			//Se genera el numero de la bola
+			$numero=genNumAle();
+			//Comprobacion de que no este la bola en el array
+			if(!in_array($numero,$arrayCarton)){
+				//Despues de comprobar que no esta repetida la bola, se añade al carton
+				$arrayCarton[$i]=$numero;
+				$i++;
+			}
+			//Cuando termina de rellenase el array del carton
+			//Se le agrega en el ultimo valor un el contador de bolas que han salido que empezara en 0
+			//Cambiable a un array boolean
+			if(count($arrayCarton)==$maximo){
+				$arrayCarton[$maximo]=0;
+			}
+			
+		}/**/
+		
+		//Posible codigo con ArrayBoolean
+		/*
+		//Indica el array numerico del carton
+		$num=0;
+		//Indica el array boolean del carton
+		$boolean=1;
+		$arrayCarton[$boolean]=genArrayBoolean($maximo);
+		while(count($arrayCarton[$num])<$maximo){
+			//Se genera el numero de la bola
+			$numero=genNumAle();
+			//Comprobacion de que no este la bola en el array
+			if(!in_array($numero,$arrayCarton[$num])){
+				//Despues de comprobar que no esta repetida la bola, se añade al carton
+				$arrayCarton[$num][$i]=$numero;
+				$i++;
+			}
+		}
+		/**/
+		return $arrayCarton;
+	}
+	function genArrayBoolean($tamaño){
+		$esarray=array();
+		for($i=0;$i<$tamaño;$i++){
+			$esarray[$i]=false;
+		}
+		return $esarray;
+	}
+	function mostrarCartones($array){
+		//Creo una tabla por cada jugador
+		echo "<table>";
+		foreach($array as $carton){
+			echo "<tr>";
+			foreach($carton as $valor){
+				if($valor!=0)
+					echo "<td>".$valor."</td>";
+			}
+			echo "</tr>";
+		}
+		echo "</table><br/>";
+	}
+	function contarBoolean($array){
+		$cont=0;
+		for($i=0;$i<count($array);$i++){
+			if($array[$i])
+				$cont++;
+		}
+		return $cont;
+	}
+	
 	//Nombre de la carpeta que tiene las imagenes, por si hiciera falta cambiarla
 	$carptImg="./DWES_Reto1_Bingo_ImagesBolas/";
 	$formatoImg=".PNG";
 	//Se que los usuarios habria una mejor forma de hacerlo mas dinamico
 	//Defino las variables y los arrays que se usaran en el juego
 	$cartones=3;
-	$arrayJugador1=array();
-	$arrayJugador2=array();
-	$arrayJugador3=array();
-	$arrayJugador4=array();
-	$jugadores=array($arrayJugador1,$arrayJugador2,$arrayJugador3,$arrayJugador4);/**/
+	$jugadores=array();/**/
+	$maxJuga=4;
 	$maxCarton=15;
 	
 	echo "<h4>Generando cartones</h4>";
 	$complet=false;
 	$i=0;
 	$j=0;
-	while(!$complet){
+	while(!$complet) {
 		$k=0;
-		$arrayCarton=array();
-		while(count($arrayCarton)<$maxCarton){
-			//Se genera el numero de la bola
-			$numero=rand(1,60);
-			//Comprobacion de que no este la bola en el array
-			if(!in_array($numero,$arrayCarton)){
-				//Despues de comprobar que no esta repetida la bola, se añade al carton
-				$arrayCarton[$k]=$numero;
-				$k++;
-			}
-			//Antiguo codigo, esta mal dado que no hace la comprobacion
-			/*if($k<count($arrayCarton)){
-				$l=0;
-				while($l<count($arrayCarton)&&$numValido){
-					if($arrayCarton[$k]==$numero){
-						$numValido=false;
-					}
-					$l++;
-				}
-			}/**/
-			/*if($numValido){
-				$arrayCarton[$k]=$numero;
-				$k++;
-			}/**/
-			//Cuando termina de rellenase el array del carton
-			//Se le agrega en el ultimo valor un el contador de bolas que han salido que empezara en 0
-			if(count($arrayCarton)==$maxCarton){
-				$arrayCarton[$maxCarton]=0;
-			}/**/
-		}
 		// Se añade el carton al jugador
+		$arrayCarton=array();
+		$arrayCarton=genCarton($arrayCarton,$maxCarton);
 		$jugadores[$i][$j]=$arrayCarton;
 		$j++;
 		//En el caso de que ya tenga los cartones el jugador, paso al siguiente
@@ -79,38 +118,28 @@ echo "
 			$j=0;
 		}
 		//Una vez que todos los jugadores tengan sus cartones, paro el bucle
-		if($i==count($jugadores)){
+		if($i==$maxJuga){
 			$complet=true;
 		}
 	}
 	//Muestra los arrays de los jugadores y sus cartones
 	for($i=0;$i<count($jugadores);$i++){
 		echo "El jugador ".($i+1)." tiene los siguientes cartones<br/>";
-		echo "<table>";
-		for($j=0;$j<count($jugadores[$i]);$j++){
-			echo "<tr>";
-			for($k=0;$k<(count($jugadores[$i][$j])-1);$k++){
-				echo "<td>".$jugadores[$i][$j][$k]."</td>";
-			}
-			echo "</tr>";
-		}
-		echo "</table><br/>";
+		echo mostrarCartones($jugadores[$i]);
 	}
-	
+
 	echo "<h4>Sacar bolas del bombo</h4>";
 	//Inicializo el control para saber las bolas que han salido 
 	$esbingo=false;
 	$cont=60;
 	$bombo=((bool)array());
-	for($i=1;$i<($cont+1);$i++){
-		$bombo[$i]=false;
-	}
+	$bombo=genArrayBoolean($cont+1);
 	$bombo[0]=true;
 	$ganador="";
 	//Empieza el bucle para sacar bolas del bombo
 	while($cont>0&&!$esbingo){
 		//Genero un numero
-		$numero=rand(1,60);
+		$numero=genNumAle();
 		//Compruebo que no este repetido el valor en el array $bombo
 		if(!$bombo[$numero]){
 			//Al no estar repetido, lo defino como que ya ha aparecido
@@ -135,8 +164,32 @@ echo "
 								$ganador=$ganador." El jugador ".($i+1)." ha ganado con el carton ".($j+1).".";
 							}
 						}
+						
+						$k++;
+					}/**/
+					//Comprobacion para array boolean
+					/*
+					//Indica el array numerico del carton
+					$num=0;
+					//Indica el array boolean del carton
+					$boolean=1;
+					$esencontrado=false;
+					while($k<(count($jugadores[$i][$j][$num]))&&!$esencontrado) {
+						if($jugadores[$i][$j][$num][$k]==$numero) {
+						//echo "El jugador ".($i+1)." tiene el numero ".$numero." y lleva ".contarBoolean($jugadores[$i][$j][$boolean][$k])." numeros<br/>";
+							$esencontrado=true;
+							$jugadores[$i][$j][$boolean][$k]=true;
+							//Compruebo el ultimo valor que tiene el array donde dice 
+							// la cantidad de numero que tiene el usuario
+							if(contarBoolean($jugadores[$i][$j][$boolean][$k])==$maxCarton){
+								$esbingo=true;
+								$ganador=$ganador." El jugador ".($i+1)." ha ganado con el carton ".($j+1).".";
+							}
+						}
+						
 						$k++;
 					}
+					/**/
 				}
 			}
 			$cont--;
