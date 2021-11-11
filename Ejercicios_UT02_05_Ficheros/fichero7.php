9dbd53464dcdac3faa3c6c0beace8ed8e9aff7d2
SantiagoSanguino <91522514+SantiagoSanguino@users.noreply.github.com>
2021-11-11 09:35:05 +0100

    Add files via upload

--- Ejercicios_UT02_05_Ficheros/fichero7.php
+++ Ejercicios_UT02_05_Ficheros/fichero7.php
@@ -1,28 +1,51 @@
 <?php
 
-	if(!empty($_POST["fichori"])&&!empty($_POST["fichact"])) {
+	if((!empty($_POST["fichori"])||!empty($_POST["fichdest"]))&&!empty($_POST["fichact"])) {
 		$mensaje="";
 		/* Nombre y/o ruta del fichero */
 		$fichero1=$_POST["fichori"]; //Fichero Origen
+		$fichero2=$_POST["fichdest"]; // Fichero destino
 		if($_POST["fichact"]=="copyfich") {
 			// Copiar fichero
 			/* Compruebo si han escrito algo en el destino y si existe el fichero origen*/
-			if(!empty($_POST["fichdest"])&&(file_exists($fichero1)==1)) {
-				$fichero2=$_POST["fichdest"]; // Fichero destino
+			if(!empty($_POST["fichori"])&&!empty($_POST["fichdest"])&&(file_exists($fichero1)==1)) {
 				/* Compruebo si son fichero los dos valores que me pasan*/
 				if(is_file($fichero1)) {
-					if(!is_file($fichero2)){
+					$direct=substr($fichero2,0,strrpos($fichero2,"\\"));
+					if(file_exists($direct)==0){
 						/* En el caso de que no exista lo creo*/
+						//El directorio seria con mkdir
+						mkdir($direct);
+						$mensaje=$mensaje."Directorio creado<br/>";
 					}
-					$mensaje=$mensaje."Son ficheros";
-				}else {
-					$mensaje=$mensaje."No son ficheros";
+					//En el caso de que exista solo copia
+					/* Se deben usar las \ para crear definir los directorios*/
+					copy($fichero1,$fichero2);
+					$mensaje=$mensaje."Archivo copiado correctamente<br/>";
 				}
+			}else {
+				$mensaje=$mensaje."No se puede copiar";
 			}
 		}else if($_POST["fichact"]=="renamefich") {
 			// Renombrar fichero
+			if(!empty($_POST["fichori"])&&!empty($_POST["fichdest"])) {
+				$fichero2=$_POST["fichdest"]; // Nuevo nombre
+				rename($fichero1,$fichero2);
+				$mensaje=$mensaje."Se ha renombrado correctamente<br/>";
+			}else {
+				$mensaje=$mensaje." No se puede renombrar el archivo<br/>";
+			}
 		}else if($_POST["fichact"]=="deletfich") {
 			// Borrar fichero
+			if(!empty($_POST["fichori"])&&file_exists($fichero1)==1){
+				unlink($fichero1);
+				$mensaje=$mensaje."Se ha borrado correctamente<br/>";
+			}else if(!empty($_POST["fichdest"])&&file_exists($fichero2)==1){
+				unlink($fichero2);
+				$mensaje=$mensaje."Se ha borrado correctamente<br/>";
+			}else{
+				$mensaje=$mensaje."No se puede borrar el fichero<br/>";
+			}
 		}
 		echo $mensaje;
 	}
@@ -31,9 +54,9 @@
 	if(empty($_POST["fichori"])||empty($_POST["fichact"])) {
 		echo "<form method=\"post\" action=\"".htmlspecialchars($_SERVER["PHP_SELF"])."\">
 				<br/>
-				<label for=\"fichori\">Fichero Origen(Path/nombre): </label>
+				<label for=\"fichori\">Fichero Origen (Path/nombre): </label>
 				<input type=\"text\" name=\"fichori\" /> </br>
-				<label for=\"fichdest\">Fichero Destino(Path/nombre): </label>
+				<label for=\"fichdest\">Fichero Destino (Path/nombre): </label>
 				<input type=\"text\" name=\"fichdest\" /> </br></br>
 				<legend>Operaciones</legend>
 				<input type=\"radio\" value=\"copyfich\" name=\"fichact\" /> Copiar fichero </br>
